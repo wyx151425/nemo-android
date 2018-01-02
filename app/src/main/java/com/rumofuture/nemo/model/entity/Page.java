aade33b9c8bd3cd1c5f2b7da7e249d27eb6e6e77
wyx151425 <wyx151425@foxmail.com>
2018-01-02 20:00:34 +0800

    2018/01/02

--- app/src/main/java/com/rumofuture/nemo/model/entity/Page.java
+++ app/src/main/java/com/rumofuture/nemo/model/entity/Page.java
@@ -5,4 +5,23 @@ package com.rumofuture.nemo.model.entity;
  */
 
 public class Page extends NemoEntity {
+
+    private Book book;  // 所属漫画册
+    private String imageUrl;  // 图像
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
--- app/src/main/java/com/rumofuture/nemo/model/entity/User.java
+++ app/src/main/java/com/rumofuture/nemo/model/entity/User.java
@@ -4,7 +4,15 @@ package com.rumofuture.nemo.model.entity;
  * Created by WangZhenqi on 2018/1/1.
  */
 
-public class User extends NemoUser {
+public class User extends NemoEntity {
+
+    private String name;  // 姓名
+    private String email;  // 电子邮箱
+    private String mobilePhoneNumber;  // 手机号
+    private String password;  // 密码
+    private Integer status;  // 状态：0-禁用用户/1-普通用户/2-普通可发布漫画用户
+
+    private String token;
 
     private String motto;  // 座右铭
     private String profile;  // 简介
@@ -22,6 +30,54 @@ public class User extends NemoUser {
 
     private String avatarUrl;  // 头像
     private String portraitUrl;  // 个人肖像
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobilePhoneNumber() {
+        return mobilePhoneNumber;
+    }
+
+    public void setMobilePhoneNumber(String mobilePhoneNumber) {
+        this.mobilePhoneNumber = mobilePhoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 
     public String getMotto() {
         return motto;
